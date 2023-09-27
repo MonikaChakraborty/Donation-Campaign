113d6772b6d556de7a4aafd8f7e1196d545d526c
MonikaChakraborty <mchakraborty7516@gmail.com>
2023-09-27 22:57:33 +0600

    search category on the banner section is done

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,24 +1,36 @@
 import '../../../Resources/banner.jpeg'
-const Banner = () => {
+import { useState } from "react";
+
+const Banner = ({ setSearchCategory }) => {
+    const [inputCategory, setInputCategory] = useState("");
+
+    const handleSearchCategory = () => {
+        setSearchCategory(inputCategory);
+    }
+
     return (
         <div className="">
-            <div className="flex h-screen justify-center ">
-    <div
-        className="w-full h-[75vh] bg-[url('Resources/banner.jpeg')] bg-cover bg-center">
-            
-        <div className="w-full h-full flex justify-center items-center backdrop-brightness-50 flex-col">
-        <div className="absolute inset-0 bg-white opacity-80"></div>
-        <h1 className='text-[#0B0B0B] text-4xl font-semibold mb-3  relative'>I Grow By Helping People In Need</h1>
-       <div className=' text-center flex justify-center items-center'>
-        
-       <input type="text" placeholder='Search here....' className="pl-2 pr-20 py-3 border border-solid border-slate-300 border-r-0  relative  rounded-l-lg" />
-        <span className="text-white text-lg font-medium p-2.5 px-5 border border-[#FF444A] bg-[#FF444A] border-l-0 rounded-r-lg relative">Search</span>
-
-       </div>
-                
-        </div>
-    </div>
-</div>
+            <div className="flex h-screen justify-center">
+                <div className="w-full h-[75vh] bg-[url('Resources/banner.jpeg')] bg-cover bg-center">
+                    <div className="w-full h-full flex justify-center items-center backdrop-brightness-50 flex-col">
+                        <div className="absolute inset-0 bg-white opacity-80"></div>
+                        <h1 className='text-[#0B0B0B] text-4xl font-semibold mb-3 relative'>I Grow By Helping People In Need</h1>
+                        <div className='text-center flex justify-center items-center'>
+                            <input
+                                type="text"
+                                placeholder='Search here....'
+                                className="pl-2 pr-20 py-3 border border-solid border-slate-300 border-r-0 relative rounded-l-lg"
+                                value={inputCategory}
+                                onChange={item => setInputCategory(item.target.value)}/>
+                            <span
+                                onClick={handleSearchCategory}
+                                className="text-white text-lg font-medium p-2.5 px-5 border border-[#FF444A] bg-[#FF444A] border-l-0 rounded-r-lg relative">
+                                Search
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,24 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Header/Banner/Banner";
 import Categories from "../../components/Header/Categories/Categories";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+    const categories = useLoaderData();
+    const [searchCategory, setSearchCategory] = useState("");
+    const [filteredCategories, setFilteredCategories] = useState([]);
 
-    const categories = useLoaderData()
+    useEffect(() => {
+        const filteredCategory = categories.filter(category =>
+            category.category_name.toLowerCase().includes(searchCategory.toLowerCase())
+        );
+        setFilteredCategories(filteredCategory);
+    }, [searchCategory, categories]);
 
-    // console.log(categories);
     return (
         <div>
-            <Banner></Banner>
-
-            
-            <Categories categories = {categories}></Categories>
+            <Banner setSearchCategory={setSearchCategory}></Banner>
+            <Categories categories={searchCategory ? filteredCategories : categories}></Categories>
         </div>
     );
 };
